4be0850878af3baf3c749ffe3eb32604d23f10ea
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-02-28 21:30:46 -0300

    devCommit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,8 @@ import styles from './styles.module.scss';
 import Image from 'next/image';
 import logoImg from '../../../public/images/logo.svg';
 
+import { SignInButton } from '../SignInButton';
+
 export function Header() {
   return (
     <header className={styles.headerContainer}>
@@ -12,6 +14,8 @@ export function Header() {
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+        
+        <SignInButton />
       </div>
     </header>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
-import styles from '../../styles/home.module.scss';
+import Image from 'next/image';
+import girlCodingImg from '../../public/images/avatar.svg';
+
+import styles from './home.module.scss';
+
 import Head from 'next/head';
 
 export default function Home() {
@@ -6,12 +10,21 @@ export default function Home() {
     <>
       {/* tudo que eu jogar dentro deste head aqui sera anexado no head la do document */}
       <Head>
-        <title>inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>
-        Hello World
-      </h1>
+      <main className={styles.contentContainer} >
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $9.98 month</span>
+          </p>
+        </section>
+
+        <Image src={girlCodingImg} alt="Girl coding" />
+      </main>
     </>
   )
 }
