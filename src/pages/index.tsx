6fd940332f91b608fa5a1ed76fbc8f8727fffa8e
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-02-28 22:16:54 -0300

    devCommit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from 'next';
 import Image from 'next/image';
 import girlCodingImg from '../../public/images/avatar.svg';
 
@@ -5,8 +6,16 @@ import styles from './home.module.scss';
 
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       {/* tudo que eu jogar dentro deste head aqui sera anexado no head la do document */}
@@ -20,14 +29,35 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all publications <br />
-            <span>for $9.98 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
                
         <Image src={girlCodingImg} alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KYJtlIFVJyNdgHHeH4jW3dC', {
+    expand: ['product'] // Utiliza o 'expand' para pegar todos os dados do produto
+  });
+  
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency', 
+      currency: 'USD' 
+    }).format(price.unit_amount / 100),
+
+  };
+  
+  return {
+    props: {
+      product
+    }
+  }
 }
