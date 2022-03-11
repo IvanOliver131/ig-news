c52098f6f9ba415c8b857e7638e9b7f37471d769
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-03-11 08:53:23 -0300

    devCommit

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -7,7 +7,7 @@ import styles from './styles.module.scss';
 export function SignInButton() {
   const { data: session } = useSession();
 
-  // Se existir a session apresenta o botao 1 senao utiliza o botao 2
+  // Se existir a session apresenta o button 1 senao utiliza o button 2
   return session ? (
     <button 
       type="button"
