510445d8b32ab1535293af1d0cd778e4adde33b6
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-03-21 22:48:53 -0300

    devCommit

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 import { version } from '../../package.json';
 
-// * Config do stripe * //
+// * Config  stripe * //
 export const stripe = new Stripe(
   process.env.STRIPE_API_KEY,
   {
