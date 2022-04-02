c07de7ac8ef255436c47e31601cf6a893fb02a71
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-04-02 16:09:34 -0300

    devCommit

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,4 +14,26 @@ export default NextAuth({
       authorization: {params: { scope: 'read:user'}}
     }),
   ],
+  jwt: {
+   secret: process.env.SIGNIN_KEY,
+  },
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+      
+      try {
+        // Query faunaDB
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }     
+    }
+  }
 })
