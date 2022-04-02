87a652ea2766c15f0cc3cc9272eddf9dd9c5495a
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-04-02 17:33:20 -0300

    devCommit

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getSession } from 'next-auth/client'
+import { getSession } from "next-auth/react"
 import { stripe } from "../../services/stripe";
 
 export default async function subscribe(req: NextApiRequest, res: NextApiResponse) {
