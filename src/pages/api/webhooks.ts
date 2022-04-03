638417c004544fb4186554f38f8c4df541ae1177
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-04-03 12:48:14 -0300

    devCommit

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -23,7 +23,10 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-  'checkout.session.completed'
+  'checkout.session.completed',
+  // 'customer.subscription.created',
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
 ])
 
 export default async function webhooks (req: NextApiRequest, res: NextApiResponse) {
@@ -48,13 +51,28 @@ export default async function webhooks (req: NextApiRequest, res: NextApiRespons
     if (relevantEvents.has(type)) {
       try {
         switch (type) {
+          // case 'customer.subscription.created':
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+              // type === 'customer.subscription.created',
+            );
+
+            break;
           case 'checkout.session.completed':
             
             const checkoutSession = event.data.object as Stripe.Checkout.Session;
             
             await saveSubscription(
               checkoutSession.subscription.toString(),
-              checkoutSession.customer.toString()
+              checkoutSession.customer.toString(),
+              true
             )
 
             break;
