3f7ddc884ed254482b6908269bf80f89fd10121b
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-04-09 13:11:04 -0300

    devCommit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,11 @@
 import styles from './styles.module.scss';
+import Link from 'next/link';
 
 import Image from 'next/image';
 import logoImg from '../../../public/images/logo.svg';
 
 import { SignInButton } from '../SignInButton';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
   return (
@@ -11,8 +13,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <Image src={logoImg} alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/" prefetch>
+            <a>Home</a>
+          </ActiveLink>    
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         
         <SignInButton />
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,8 +3,20 @@ import Prismic from '@prismicio/client';
 import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
+import { RichText } from 'prismic-dom';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -13,21 +25,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href='#'>
-            <time>12 de março de babbablalb</time>
-            <strong>LBALBLALBALBÇLAABLALBLA</strong>
-            <p>AJKLSDÇFJLKASDFKLASDLKFJLKASDJFLKASDJLFKJASDLKFJASLDKFJLASDKJFLKSDAJLFKDSAJLFK</p>
-          </a>
-          <a href='#2'>
-            <time>12 de março de babbablalb</time>
-            <strong>LBALBLALBALBÇLAABLALBLA</strong>
-            <p>AJKLSDÇFJLKASDFKLASDLKFJLKASDJFLKASDJLFKJASDLKFJASLDKFJLASDKJFLKSDAJLFKDSAJLFK</p>
-          </a>
-          <a href='#3'>
-            <time>12 de março de babbablalb</time>
-            <strong>LBALBLALBALBÇLAABLALBLA</strong>
-            <p>AJKLSDÇFJLKASDFKLASDLKFJLKASDJFLKASDJLFKJASDLKFJASLDKFJLASDKJFLKSDAJLFKDSAJLFK</p>
-          </a>
+          { posts.map(post => (
+            <a key={post.slug}href='#'>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -40,18 +44,27 @@ export const getStaticProps: GetStaticProps = async () => {
   // Quando for criar um custom type utilize o nome no 
   // lugar desse 2 -------- usei o 2 porque não sabia exatamente o que iria usar
 
-  const response = await prismic.query([
+  const response = await prismic.query<any>([
     Prismic.predicates.at('document.type', '2')
   ], {
     fetch: ['title', 'content'],
     pageSize: 100,
   });
 
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    };
+  })
 
-  console.log(JSON.stringify(response, null, 2));
   return {
-    props: {}
+    props: { posts }
   }
-
-
 }
