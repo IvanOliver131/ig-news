3e9baca27570a18d22ee9d894e5cbd75d2c93d2c
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-12-05 10:24:29 -0300

    fix: teste

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
-import { SignInButton } from '../SignInButton';
-import { ActiveLink } from '../ActiveLink';
+import { SignInButton } from "../SignInButton";
+import { ActiveLink } from "../ActiveLink";
+import Image from "next/image";
 
 // import Image from 'next/image';
 // import logoImg from '../../../public/images/logo.svg';
@@ -12,17 +13,21 @@ export function Header() {
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         {/* <Image src={logoImg} alt="ig.news" /> */}
-        <img src='../../../public/images/logo.svg' alt="ig-news" />
+        <Image src="../../../public/images/logo.svg" alt="ig-news" />
         <nav>
           {/* os prefetchs eram apenas "prefetch" */}
           <ActiveLink activeClassName={styles.active} href="/" prefetch={false}>
             <a>Home</a>
-          </ActiveLink>    
-          <ActiveLink activeClassName={styles.active} href="/posts" prefetch={false}>
+          </ActiveLink>
+          <ActiveLink
+            activeClassName={styles.active}
+            href="/posts"
+            prefetch={false}
+          >
             <a>Posts</a>
           </ActiveLink>
         </nav>
-        
+
         <SignInButton />
       </div>
     </header>
